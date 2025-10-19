d0b559e45a3c0d7756a0ce5329c2e0a73eb2f3b6
Сеноженский Вячеслав Владимирович <com.cerety.vepl555@gmail.com>
2025-10-19 19:09:50 +0300

    Implemented Repository pattern for Publications and Teachers

---
+++ web-app/PortalNauchnyhPublikatsiy/PortalNauchnyhPublikatsiy.Application/Interfaces/ITeacherRepository.cs
@@ -0,0 +1,15 @@
+﻿using PortalNauchnyhPublikatsiy.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalNauchnyhPublikatsiy.Application.Interfaces
+{
+    public interface ITeacherRepository
+    {
+        Task<IEnumerable<Teacher>> GetAllAsync();
+        Task<Teacher?> GetByIdAsync(int id);
+    }
+}
--- web-app/PortalNauchnyhPublikatsiy/PortalNauchnyhPublikatsiy/Program.cs
+++ web-app/PortalNauchnyhPublikatsiy/PortalNauchnyhPublikatsiy/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using PortalNauchnyhPublikatsiy.Infrastructure.Data;
+using PortalNauchnyhPublikatsiy.Application.Interfaces;
+using PortalNauchnyhPublikatsiy.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,8 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
+builder.Services.AddScoped<IPublicationRepository, PublicationRepository>();
+builder.Services.AddScoped<ITeacherRepository, TeacherRepository>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
