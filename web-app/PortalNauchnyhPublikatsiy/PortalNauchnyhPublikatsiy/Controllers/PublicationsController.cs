89841694f4f0185f15837bcf834c9c293c18b87b
Сеноженский Вячеслав Владимирович <com.cerety.vepl555@gmail.com>
2025-11-16 17:42:48 +0300

    v0.6: Created Publications list page (Controller and View)

---
+++ web-app/PortalNauchnyhPublikatsiy/PortalNauchnyhPublikatsiy/Controllers/PublicationsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortalNauchnyhPublikatsiy.Application.Services;
+
+namespace PortalNauchnyhPublikatsiy.Web.Controllers
+{
+    public class PublicationsController : Controller
+    {
+        private readonly IPublicationService _publicationService;
+
+        public PublicationsController(IPublicationService publicationService)
+        {
+            _publicationService = publicationService;
+        }
+
+        // Этот метод будет вызываться, когда пользователь перейдет по адресу /Publications/Index
+        public async Task<IActionResult> Index()
+        {
+            // 1. Обращаемся к сервису за списком всех публикаций (в виде DTO)
+            var publications = await _publicationService.GetAllPublicationsAsync();
+
+            return View(publications);
+        }
+    }
+}
